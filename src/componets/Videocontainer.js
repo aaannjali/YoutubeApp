7cc0ee1d0f0bf8b672bdc7a39453b9ff42fe87ac
Anjali Sharma <121468990+aaannjali@users.noreply.github.com>
2024-01-25 14:42:11 +0530

    initial commit

--- src/componets/Videocontainer.js
+++ src/componets/Videocontainer.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { YOUTUBE_API } from '../utils/constant';
+import VideoCart from './VideoCart';
 
 const Videocontainer = () => {
+  const [videos, setvideos] = useState([]);
+
+  useEffect( ()=> {
+   getVideos();
+  },[]);
+
+  const getVideos = async () =>{
+    const data = await fetch(YOUTUBE_API);
+    const json = await data.json();
+    setvideos(json.items);
+  };
+
   return (
-    <div>
-      
+    <div className='flex flex-wrap'>
+      {videos.map(videos => <VideoCart key={videos.id} info={videos} />)}
+     
     </div>
   )
 }
---
+++ src/utils/constant.js
@@ -0,0 +1,4 @@
+
+const API_KEY = "AIzaSyD7MbVi8yqitE98nj3X8EApdeIjc0wIUgU";
+
+export const YOUTUBE_API = " https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + API_KEY;
